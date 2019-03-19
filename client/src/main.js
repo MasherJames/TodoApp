3d0fde5d7dfafd62ec88e5f84f5f37dde111a548
James Macharia <jamesmasher13@gmail.com>
2019-03-19 11:22:09 +0300

    Add all

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import jwtDecode from "jwt-decode";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -7,6 +8,11 @@ import setAuthToken from "./services/setAuthToken";
 const token = localStorage.getItem("token");
 if (token) {
   setAuthToken(token);
+  const decoded = jwtDecode(token);
+  // current time exceeds time in session, user should be logged out
+  if (decoded.exp < Date.now() / 1000) {
+    store.dispatch("logout").then(() => router.push("/login"));
+  }
 }
 
 Vue.config.productionTip = false;
--- client/src/views/Todos.vue
+++ client/src/views/Todos.vue
@@ -15,7 +15,7 @@ export default {
   },
   computed: mapGetters(["allTodos"]),
   created() {
-    const res = this.fetchTodos();
+    this.fetchTodos();
   }
 };
 </script>
